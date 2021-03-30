5d6c8846d954e8ad513c22d86ba06c4a3ce39ad1
Lucas Bernardo <lucasbernardo95@gmail.com>
2021-03-30 00:43:52 -0300

    Aplicando o princípio da responsabilidade única

---
+++ solid/src/main/java/com/lucas/rn/solid/service/ReajusteService.java
@@ -0,0 +1,22 @@
+package com.lucas.rn.solid.service;
+
+import com.lucas.rn.solid.ValidacaoException;
+import com.lucas.rn.solid.model.Funcionario;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class ReajusteService {
+
+    public void reajustarSalario(Funcionario funcionario, BigDecimal aumento) {
+        BigDecimal salarioAtual = funcionario.getSalario();
+
+        BigDecimal percentualReajuste = aumento.divide(salarioAtual, RoundingMode.HALF_UP);
+        if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
+            throw new ValidacaoException("Reajuste não pode ser superior a 40% do salário!");
+        }
+        BigDecimal salarioAjustado = salarioAtual.add(aumento);
+        funcionario.atualizarSalario(salarioAjustado);
+    }
+
+}

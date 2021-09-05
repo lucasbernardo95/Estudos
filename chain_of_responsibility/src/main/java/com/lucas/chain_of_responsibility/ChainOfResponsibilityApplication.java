ab4d65f84dfda39d2009e7af2e1a2687265fd304
lucasbernardo95 <lucasbernardo95@gmail.com>
2021-09-05 13:37:08 -0300

    Remoção de classe desnecessária

--- chain_of_responsibility/src/main/java/com/lucas/chain_of_responsibility/ChainOfResponsibilityApplication.java
+++ chain_of_responsibility/src/main/java/com/lucas/chain_of_responsibility/ChainOfResponsibilityApplication.java
@@ -1,13 +1,17 @@
 package com.lucas.chain_of_responsibility;
 
+import com.lucas.chain_of_responsibility.imccalculo.IMCCalculo;
+import com.lucas.chain_of_responsibility.model.IMC;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
 public class ChainOfResponsibilityApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ChainOfResponsibilityApplication.class, args);
+        IMC imc = new IMC(84, 1.81);
+        System.out.println(IMCCalculo.calcularImc(imc));
+//        SpringApplication.run(ChainOfResponsibilityApplication.class, args);
     }
 
 }
